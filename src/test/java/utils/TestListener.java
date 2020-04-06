4176e06f4c5a524501b5e793d9f17c6f7f968344
m_idolenka <idolenkomaria1@gmail.com>
2020-04-06 18:52:34 +0300

    SalesforceAllurefix1

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,8 +1,6 @@
 package utils;
 
-import org.openqa.selenium.NoSuchSessionException;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -23,24 +21,24 @@ public class TestListener implements ITestListener {
     public void onTestFailure(ITestResult iTestResult) {
         System.out.println(String.format("======================================== FAILED TEST %s Duration: %ss ========================================", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
-        takeScreenshot(iTestResult);
+//        takeScreenshot(iTestResult);
     }
 
     public void onTestSkipped(ITestResult iTestResult) {
         System.out.println(String.format("======================================== SKIPPING TEST %s ========================================", iTestResult.getName()));
-        takeScreenshot(iTestResult);
+//        takeScreenshot(iTestResult);
     }
 
-    //    @Attachment(value = "Last screen state", type = "image/png")
-    private byte[] takeScreenshot(ITestResult iTestResult) {
-        try {
-            return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
-        } catch (NoSuchSessionException ex) {
-            return null;
-        } catch (IllegalStateException ex) {
-            return null;
-        }
-    }
+//    @Attachment(value = "Last screen state", type = "image/png")
+//    private byte[] takeScreenshot(ITestResult iTestResult) {
+//        try {
+////            return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+//        } catch (NoSuchSessionException ex) {
+//            return null;
+//        } catch (IllegalStateException ex) {
+//            return null;
+//        }
+//    }
 
 
     private long getExecutionTime(ITestResult iTestResult) {
